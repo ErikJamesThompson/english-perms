0c80ae2fec989fb2bb612aee7b9d459356ddae29
Erik Thompson <erik.james.thompson@gmail.com>
2024-02-11 21:34:22 -0800

    uses checkWord effectively

--- dist/index.js
+++ dist/index.js
@@ -1,22 +1,27 @@
 import en from "dictionary-en";
-function acceptScring(s) {
-    const realWorldWords = {};
-    generatePermutationsRecursively(s, realWorldWords);
-    console.log(en);
+import nspell from "nspell";
+export default function acceptScring(s) {
+    const permutations = new Map();
+    let realWorldWords = [];
+    console.log(process.argv);
+    generatePermutationsRecursively(s, permutations);
+    realWorldWords = checkWordsAreReal(permutations);
     return realWorldWords;
 }
 function generatePermutationsRecursively(s, permutations) {
     function generatePermutations(constructedString, remainingCharacters) {
         // add permutation if exists and exit out of current call
         // I like more explicit return rather than else (more readable)
-        if (remainingCharacters.length === 0 && !permutations[constructedString]) {
-            permutations[constructedString] = true;
+        if (remainingCharacters.length === 0 &&
+            !permutations.get(constructedString)) {
+            permutations.set(constructedString, true);
             return;
         }
         for (let i = 0; i < remainingCharacters.length; i++) {
             // add character at node in tree if it doesn't already exist and not ''
-            if (!permutations[constructedString] && constructedString.length > 0) {
-                permutations[constructedString] = true;
+            if (!permutations.get(constructedString) &&
+                constructedString.length > 0) {
+                permutations.set(constructedString, true);
             }
             //   pull out letter and append to currently generated string
             const constructedStringWithNewPivot = constructedString + remainingCharacters[i];
@@ -29,4 +34,18 @@ function generatePermutationsRecursively(s, permutations) {
     generatePermutations("", s);
     return permutations;
 }
-console.log(acceptScring("old"));
+function checkWordsAreReal(permutations) {
+    const correctWords = [];
+    console.log(en);
+    const spell = nspell(en);
+    for (let word of permutations.keys()) {
+        // spellcheck dictionary registers single letters as positives
+        // TODO find a better library that is still fast
+        const isOnlyAcceptableSingleCharacter = word.length === 1 && word === "a";
+        const isValidEnglishWord = word.length > 1 && spell.correct(word);
+        if (isValidEnglishWord || isOnlyAcceptableSingleCharacter) {
+            correctWords.push(word);
+        }
+    }
+    return correctWords;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
-import en from "dictionary-en";
 import nspell from "nspell";
+import en from "dictionary-en";
+import checkWord from "check-word";
 
 function acceptScring(s: string) {
-  const realWorldWords: { [key: string]: any } = {};
-  generatePermutationsRecursively(s, realWorldWords);
+  const permutations: Map<string, boolean> = new Map();
+  let realWorldWords: string[] = [];
 
-  const spell = nspell(en);
-  console.log(spell.correct("color"));
-  console.log(spell.correct("colour"));
+  generatePermutationsRecursively(s, permutations);
+  realWorldWords = checkWordsAreReal(permutations);
+
   return realWorldWords;
 }
 
 function generatePermutationsRecursively(
   s: string,
-  permutations: { [key: string]: boolean }
+  permutations: Map<string, boolean>
 ) {
   function generatePermutations(
     constructedString: string,
@@ -21,14 +22,20 @@ function generatePermutationsRecursively(
   ) {
     // add permutation if exists and exit out of current call
     // I like more explicit return rather than else (more readable)
-    if (remainingCharacters.length === 0 && !permutations[constructedString]) {
-      permutations[constructedString] = true;
+    if (
+      remainingCharacters.length === 0 &&
+      !permutations.get(constructedString)
+    ) {
+      permutations.set(constructedString, true);
       return;
     }
     for (let i = 0; i < remainingCharacters.length; i++) {
       // add character at node in tree if it doesn't already exist and not ''
-      if (!permutations[constructedString] && constructedString.length > 0) {
-        permutations[constructedString] = true;
+      if (
+        !permutations.get(constructedString) &&
+        constructedString.length > 0
+      ) {
+        permutations.set(constructedString, true);
       }
 
       //   pull out letter and append to currently generated string
@@ -48,4 +55,19 @@ function generatePermutationsRecursively(
   return permutations;
 }
 
-console.log(acceptScring("old"));
+function checkWordsAreReal(permutations: Map<string, boolean>): string[] {
+  const correctWords = [];
+  const wordChecker = checkWord("en");
+
+  for (let word of permutations.keys()) {
+    // TODO find a better library that is still fast
+    const isValidEnglishWord = wordChecker.check(word);
+
+    if (isValidEnglishWord) {
+      correctWords.push(word);
+    }
+  }
+  return correctWords;
+}
+
+console.log(acceptScring(process.argv.slice(2)[0]));
